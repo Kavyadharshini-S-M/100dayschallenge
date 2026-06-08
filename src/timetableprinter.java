2f1a4337faec18ff385f1cacc5e737ca7f90e52c
Kavyadharshini S M <smkavyadharshini@gmail.com>
2026-06-08 10:32:55 +0530

    Second commit

--- src/timetableprinter.java
+++ src/timetableprinter.java
@@ -1,5 +1,14 @@
+import java.util.*;
+
 public class timetableprinter {
     public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Hello welcome to Times Table Printer!\nPlease enter the number: ");
+    int n = sc.nextInt();
+    for (int i= 1; i<=12; i++){
+        System.out.print(n + " X " + i + " = " + n*i);
+        System.out.println();
+    }
 
     }
 }
