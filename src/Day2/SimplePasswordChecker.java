d8f182c636933049c092bf5af39e537c7012cc26
Kavyadharshini S M <smkavyadharshini@gmail.com>
2026-06-08 13:09:37 +0530

    coded simple password checker

--- src/Day2/SimplePasswordChecker.java
+++ src/Day2/SimplePasswordChecker.java
@@ -1,4 +1,28 @@
 package Day2;
 
+import java.util.*;
+
 public class SimplePasswordChecker {
+    public static void main(String[] args) {
+        String password = "789@qwerty";
+        int counter = 1;
+        int i = 3;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Password: ");
+        String input = sc.nextLine();
+
+        while(!input.equals(password) && counter <= 3){
+            counter++;
+            System.out.print("Try again " + i + " attempts left: ");
+            i--;
+            input = sc.nextLine();
+        }
+        if(counter == 3 || i == 0){
+            System.out.println("Passwords don't match! Try again after some time");
+        }
+
+        if(input.equals(password)){
+            System.out.print("Access Granted");
+        }
+    }
 }
